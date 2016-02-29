dfe5b0e6de410c18d61a9d0dc4d037dc11787c1a
CarolineSylvia <carolinesylviaa@gmail.com>
2016-02-29 15:59:02 +0530

    lab2

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,6 +1,6 @@
 //============================================================================
-// Name        : fl.cpp
-// Author      : 
+// Name        : lab2.cpp
+// Author      : caroline
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -13,7 +13,7 @@
 using namespace std;
 fstream fp;
 char *fname;
- class Student{
+class Student{
         string name,usn,branch;
         int sem;
 public:
@@ -31,20 +31,27 @@ public:
 
                 void pack(char *fname){
                         string buffer;
-                        fp.open(fname,ios::out);
+                        fp.open(fname,ios::out|ios::app);
                         stringstream ss;
                         ss<<name<<"|"<<usn<<"|"<<sem<<"|"<<branch;
                         buffer=ss.str();
+                        cout<<buffer;
                         fp<<buffer;
+                        buffer.erase();
+                        fp>>buffer;
+                        cout<<"frm file"<<buffer;
                         fp.close();
                 }
                 void unpack(char *fname){
                         string extra;
                         fp.open(fname,ios::in);
                         getline(fp,extra);
+                        cout<<extra;
                 };
                 void modify(){};
-                void search(){};
+                void search(){
+
+                };
 
 } ;
 
@@ -59,17 +66,24 @@ public:
          cin>>ch;
          switch(ch)
          {
-                 case 1:s1.read();
-                            cout<<"Enter the file name to pack data into:";
-                            cin>>fname;
-                            s1.pack(fname);
-                            break;
-                 case 2:s1.unpack();
-                            break;
-                 case 3:s1.modify();
-                            break;
-                 case 4:cout<<"Enter element to search for";
+                 case 1: s1.read();
+                         cout<<"Enter the file name to pack data into:";
+                         cin>>fname;
+                         s1.pack(&fname);
+                         break;
+                 case 2: cout<<"Enter the file name to read data from:";
+                 	 	 cin>>fname;
+                 	     s1.unpack(fname);
 
+                         break;
+                 case 3: s1.modify();
+                         break;
+                 case 4: string key;
+                	 	 cout<<"Enter element to search for";
+                 	 	 cin>>key;
+
+
+                 	 	 break;
 
          }
          return 0;
