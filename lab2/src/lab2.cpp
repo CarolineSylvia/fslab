125108dbc2fd8dad750a55ba2595723aee1e8474
CarolineSylvia <carolinesylviaa@gmail.com>
2016-02-29 14:59:41 +0530

    existing first commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : lab2.cpp
+// Name        : fl.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -7,9 +7,70 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <sstream>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+using namespace std;
+fstream fp;
+char *fname;
+ class Student{
+        string name,usn,branch;
+        int sem;
+public:
+                void read(){
+                        cout<<"Enter the name";
+                        cin>>name;
+                        cout<<"Enter the usn";
+                        cin>>usn;
+                        cout<<"Enter the sem";
+                        cin>>sem;
+                        cout<<"Enter the branch";
+                        cin>>branch;
+
 }
+
+                void pack(char *fname){
+                        string buffer;
+                        fp.open(fname,ios::out);
+                        stringstream ss;
+                        ss<<name<<"|"<<usn<<"|"<<sem<<"|"<<branch;
+                        buffer=ss.str();
+                        fp<<buffer;
+                        fp.close();
+                }
+                void unpack(char *fname){
+                        string extra;
+                        fp.open(fname,ios::in);
+                        getline(fp,extra);
+                };
+                void modify(){};
+                void search(){};
+
+} ;
+
+
+
+
+ int main() {
+         int ch;
+         Student s1;
+         cout<< "Enter your choice:"
+                         "1.pack 2.unpack 3.modify 4.search 5.exit";
+         cin>>ch;
+         switch(ch)
+         {
+                 case 1:s1.read();
+                            cout<<"Enter the file name to pack data into:";
+                            cin>>fname;
+                            s1.pack(fname);
+                            break;
+                 case 2:s1.unpack();
+                            break;
+                 case 3:s1.modify();
+                            break;
+                 case 4:cout<<"Enter element to search for";
+
+
+         }
+         return 0;
+ }
