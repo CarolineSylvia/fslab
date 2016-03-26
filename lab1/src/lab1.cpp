4a6c07d76a481559cac525469d39868e744c0eef
CarolineSylvia <CarolineSylvia@users.noreply.github.com>
2016-03-26 13:28:53 +0530

    Update lab1.cpp

--- lab1/src/lab1.cpp
+++ lab1/src/lab1.cpp
@@ -1,15 +1,39 @@
 //============================================================================
 // Name        : lab1.cpp
-// Author      : 
+// Author      : Caroline
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include<string.h>
+#include<fstream>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+using namespace std;
+fstream fp1,fp2;
+
+void reverse(char *);
+char str[30];
+
+int main()
+{
+	int ch;
+	cout<<"Enter your choice: 1.String reversal through terminal 2.String reversal through files" <<endl;
+	cin>>ch;
+	switch(ch)
+	{
+		case 1: cout<<"Enter the string:"<<endl;
+		cin>>str;
+		reverse(str);
+		break;
+		
+		case 2: fp1.open("input.txt",ios::in);
+		fp2.open("output.txt",ios::out);
+		while(!fp1.eof())
+		{
+			fp1.getline(str,)
+		}
+	}
+	
 }
