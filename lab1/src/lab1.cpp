0d4614a1513778a8e88db1c07f2162ecc7ef8668
CarolineSylvia <CarolineSylvia@users.noreply.github.com>
2016-03-26 14:21:21 +0530

    final commit

--- lab1/src/lab1.cpp
+++ lab1/src/lab1.cpp
@@ -32,8 +32,28 @@ int main()
 		fp2.open("output.txt",ios::out);
 		while(!fp1.eof())
 		{
-			fp1.getline(str,)
+			fp1.getline(str,21);
+			reverse(str);
+			fp2<<str;
 		}
+		fp1.close();
+		fp2.close();
+		break;
+		
+		default: cout<<"Wrong choice"<<endl;
+		break;
 	}
-	
+}
+
+void reverse(char *)
+{
+	int len,i;
+	len=strlen(str);
+	for(i=0;i<len/2;i++)
+	{
+		char temp=str[i];
+		str[i]=str[len-1-i];
+		str[len-1-i]=temp;
+	}
+	cout<<"The reverse of the string is:"<<str<<endl;
 }
