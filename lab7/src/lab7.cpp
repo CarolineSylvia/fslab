fd827f0172fa5812ff2d3291d05bd26ce3cfea85
CarolineSylvia <CarolineSylvia@users.noreply.github.com>
2016-03-29 15:48:16 +0530

    update 7

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -7,9 +7,57 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include<string.h>
+#include<stdlib.h>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+class filelist
+{
+	string l1[100],l2[100];
+public: void matching();
+		void load();
+
+}c;
+
+void sort(string *l,int n);
+
+void filelist::void load()
+{
+		int c1=0,c2=0;
+		fstream fp1,fp2;
+		string buffer;
+		fp1.open("data1.txt",ios::in);
+		while(fp1.eof())
+		{
+				getline(fp1,buffer);
+				l1[++c1]=buffer;
+
+		}
+		sort(l1,c1);
+		fp2.open("data2.txt",ios::in);
+		while(fp2.eof())
+		{
+				getline(fp2,buffer);
+				l1[++c2]=buffer;
+		}
+		sort(l2,c2);
+}
+
+void sort(string *l,int n)
+{ 	string temp;
+	int i,j;
+	for(i=1;i<=n;i++)
+	{
+		for(j=i=1;j<=n;j++)
+		{
+			if(l[i]<l[j])
+			{
+				temp=l[i];
+				l[i]=l[j];
+				l[j]=temp;
+			}
+		}
+	}
 }
