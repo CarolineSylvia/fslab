273977b1c9b300a8f36386599671e701708f51df
CarolineSylvia <CarolineSylvia@users.noreply.github.com>
2016-04-14 19:47:15 +0530

    final

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -1,63 +1,115 @@
 //============================================================================
 // Name        : lab7.cpp
-// Author      : 
+// Author      : carol
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include <fstream>
-#include<string.h>
+
+#include<iostream>
+#include<string>
+#include<fstream>
 #include<stdlib.h>
 
 using namespace std;
 
-class filelist
+class coseq
 {
-	string l1[100],l2[100];
-public: void matching();
-		void load();
+	public:
+		string list1[100],list2[100];
+		int c1, c2;
+		void read_lists();
+		void sort_lists();
+		void match_lists();
+};
 
-}c;
-
-void sort(string *l,int n);
-
-void filelist::void load()
+int main()
 {
-		int c1=0,c2=0;
-		fstream fp1,fp2;
-		string buffer;
-		fp1.open("data1.txt",ios::in);
-		while(fp1.eof())
-		{
-				getline(fp1,buffer);
-				l1[++c1]=buffer;
-
-		}
-		sort(l1,c1);
-		fp2.open("data2.txt",ios::in);
-		while(fp2.eof())
-		{
-				getline(fp2,buffer);
-				l1[++c2]=buffer;
-		}
-		sort(l2,c2);
+	system("clear");
+	coseq c;
+	c.read_lists();
+	c.sort_lists();
+    	cout<<"\n\nCommon Names in Both Lists Are : \n";
+	c.match_lists();
+	return 0;
 }
 
-void sort(string *l,int n)
-{ 	string temp;
-	int i,j;
-	for(i=1;i<=n;i++)
+void coseq::read_lists()
+{
+	fstream fp;
+	string name;
+
+	c1=0;
+	fp.open("1.txt",ios::in);
+	while(fp)
 	{
-		for(j=i=1;j<=n;j++)
+		getline(fp,name);		
+		list1[c1++]=name;	
+	}
+	fp.close();
+	c2 = 0;
+	fp.open("2.txt",ios::in);
+	while(fp)
+	{
+		getline(fp,name);
+		list2[c2++]=name;
+	}
+	fp.close();
+}
+
+void coseq::sort_lists()
+{
+	int i,j;
+	string temp;
+
+	for(i=0;i<c1;i++)
+	{
+		for(j=i+1;j<c1;j++)
 		{
-			if(l[i]<l[j])
+			if(list1[i]>list1[j])
 			{
-				temp=l[i];
-				l[i]=l[j];
-				l[j]=temp;
+				temp=list1[i];
+				list1[i]=list1[j];
+				list1[j]=temp;
 			}
 		}
 	}
+    
+    cout<<"\nThe Sorted Contents of List 1 : \n";
+    	for(i=0;i<=c1;i++)
+        cout<<list1[i]<<"\n";
+	for(i=0;i<c2;i++)
+	{
+		for(j=i+1;j<=c2;j++)
+		{
+			if(list2[i]>list2[j])
+			{
+				temp=list2[i];
+				list2[i]=list2[j];
+				list2[j]=temp;
+			}
+		}
+	}
+
+    cout<<"\nThe Sorted Contents of List 2 : \n";
+    for(i=0;i<=c2;i++)
+      	cout<<list2[i]<<"\n";
 }
+
+void coseq::match_lists()
+{
+	int i=0,j=0;
+	while(i<=c1 && j<=c2)
+	{
+		if(list1[i]==list2[j])
+		{
+			cout<<list1[i]<<endl;
+			i++;
+			j++;
+		}
+		if(list1[i]<list2[j])i++;
+		if(list1[i]>list2[j])j++;
+	}
+}
+
